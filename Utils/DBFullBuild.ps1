f59eea3a1e32ab74b15c8a48f8b9f2c1663ee9d6
Roedba <niklas.dominico2@gmail.com>
2025-12-02 16:27:51 +0100

    Added Database Schema

---
+++ Utils/DBFullBuild.ps1
@@ -0,0 +1,31 @@
+param(
+    [string]$ServerInstance = "CW-INTEG\MSSQLSERVER01"
+)
+
+$scripts = @(
+    @{path = Join-Path $PSScriptRoot "..\Scripts\Schema\Drop_Database.sql"; description =  "Drop existing Database"},
+    @{path = Join-Path $PSScriptRoot "..\Scripts\Schema\Create_Database.sql"; description =  "create New Database"},
+    @{path = Join-Path $PSScriptRoot "..\Scripts\Schema\ForeignKeys.sql"; description =  "create Primary Keys"}
+    @{path = Join-Path $PSScriptRoot "..\Scripts\Seed\PrimaryKeys.sql"; description =  "create Foreign Keys"}
+    @{path = Join-Path $PSScriptRoot "..\Scripts\Seed\Fill_Customer.sql"; description =  "fill Customer Data"}
+)
+
+function Invoke-SqlScript ($path, $description){
+    Write-Host "`n=== Running: $description ==="
+
+     try {
+        $sqlScript = Get-Content -Path $path -Raw
+        Invoke-Sqlcmd `
+            -Query $sqlScript `
+            -ServerInstance $ServerInstance `
+            -Database "bobshirt" `
+            -TrustServerCertificate `
+}
+catch {
+        Write-Error "$description failed: $_"
+        exit 1
+    }
+
+foreach ($script in $scripts) {
+    Invoke-SqlScript @script
+}
