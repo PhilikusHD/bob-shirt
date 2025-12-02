86f4b21ff133aff2923ca63382d2b89e97db7c06
Philip <philip.dammann06@gmail.com>
2025-12-02 15:48:04 +0100

    purge

--- Bob.WASM/wwwroot/main.js
+++
@@ -1,6 +0,0 @@
-import { dotnet } from './_framework/dotnet.js';
-
-const dotnetRuntime = await dotnet.create();
-const config = dotnetRuntime.getConfig();
-
-await dotnetRuntime.runMain(config.mainAssemblyName, []);
