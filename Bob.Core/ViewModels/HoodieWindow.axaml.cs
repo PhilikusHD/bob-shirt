5e471ab3fbf54eaabd4aad72fef25d47a2aa5158
carolinahoffmann <Carolina.Hoffmann@msg-life.com>
2025-12-03 12:40:54 +0100

    Added new icons and each window for each product #15

---
+++ Bob.Core/ViewModels/HoodieWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Bob.Core;
+
+public partial class HoodieWindow : UserControl
+{
+    public HoodieWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Bob.Core/ViewModels/TshirtWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Bob.Core;
+
+public partial class TshirtWindow : UserControl
+{
+    public TshirtWindow()
+    {
+        InitializeComponent();
+    }
+}
