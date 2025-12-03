e398b4652419114d43e48c35163b794fb45b440c
Roedba <niklas.dominico2@gmail.com>
2025-12-03 11:39:42 +0100

    Database fixed

--- Scripts/Schema/Drop_Database.sql
+++ Scripts/Schema/Drop_Database.sql
@@ -1,5 +1,16 @@
 USE master;
 
 IF DB_ID('bobshirt') IS NOT NULL
-    ALTER DATABASE bobshirt SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-DROP DATABASE IF EXISTS bobshirt;
+BEGIN
+
+    ALTER DATABASE bobshirt 
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    
+    DROP DATABASE bobshirt;
+    
+    PRINT 'Database bobshirt dropped successfully.';
+END
+ELSE
+BEGIN
+    PRINT 'Database bobshirt does not exist.';
+END
