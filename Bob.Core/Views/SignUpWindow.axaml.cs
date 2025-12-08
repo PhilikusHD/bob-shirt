730301c35ed57302e7c381745e520a603e274129
carolinahoffmann <Carolina.Hoffmann@msg-life.com>
2025-12-08 17:26:37 +0100

    Sign in and sign up #29

--- Bob.Core/Views/SignUpWindow.axaml.cs
+++ Bob.Core/Views/SignUpWindow.axaml.cs
@@ -1,7 +1,11 @@
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Bob.Core.Domain;
+using Bob.Core.Services;
+using Bob.Core.Sytems;
 using Bob.Core.Utils;
 
 namespace Bob.Core;
@@ -17,5 +21,17 @@ public partial class SignUpWindow : UserControl
     private void OnShopClick(object? sender, RoutedEventArgs e)
     {
         ViewManager.TransitionTo(nameof(MainPage));
+    }
+
+    private async void SignUpButtonClick(object? sender, RoutedEventArgs e)
+    {
+        Address address = new Address();
+        address.Street = StrasseTextbox.Text;
+        address.HouseNumber = HausnrTextbox.Text;
+        address.City = StadtTextbox.Text;
+        address.PostalCode = PlzTextbox.Text;
+
+        int addressId = await AddressService.AddressExists(address);
+        await LoginSystem.RegisterCustomerAsync(VornameTextbox.Text, NameTextbox.Text, EmailTextbox.Text, addressId, TelefonTextbox.Text, PasswortTextbox.Text);
     }
 }
