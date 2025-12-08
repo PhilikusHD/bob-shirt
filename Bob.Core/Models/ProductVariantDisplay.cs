876245515ce0e2c6cb9b831bfd3bb9b45df16cb3
Roedba <niklas.dominico2@gmail.com>
2025-12-08 19:57:18 +0100

    Product Detail View Model #21

---
+++ Bob.Core/Models/ProductVariantDisplay.cs
@@ -0,0 +1,11 @@
+﻿namespace Bob.Core.Models
+{
+    public class ProductVariantDisplay 
+    { 
+        public int VariantId { get; set; } 
+        public string Color { get; set; } 
+        public string Size { get; set; } 
+        public decimal FinalPrice { get; set; } 
+        public int Stock { get; set; } 
+    }
+}
---
+++ Bob.Core/ViewModels/ProductDetailWindowViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bob.Core.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Bob.Core.Logging;
+using Bob.Core.Domain;
+using Bob.Core.Models;
+namespace Bob.Core.ViewModels
+{
+    public partial class ProductDetailWindowViewModel : ViewModelBase
+    {
+
+        [ObservableProperty]
+        private Product currentProduct;
+
+        [ObservableProperty]
+        private ObservableCollection<ProductVariantDisplay> variants = new();
+        public ProductDetailWindowViewModel(Product product)
+        {
+
+            // Load Caps asynchronously
+            CurrentProduct = product;
+            _ = LoadProductDetailAsync();
+        }
+
+        private async Task LoadProductDetailAsync()
+        {
+            try
+            {
+                var variantEntities = await ProductService.GetVariantsForProductAsync(CurrentProduct.ProductId);
+
+                var list = new List<ProductVariantDisplay>();
+
+                foreach (var v in variantEntities)
+                {
+                    var size = await ProductService.GetSizeAsync(v.SizeId);
+                    var color = await ProductService.GetColorAsync(v.ColorId);
+                    var finalPrice = await ProductService.GetPriceAdjustedForSize(v.SizeId, CurrentProduct.Price);
+
+                    list.Add(new ProductVariantDisplay
+                    {
+                        VariantId = v.VariantId,
+                        Color = color.ColorName,
+                        Size = size.SizeName,
+                        FinalPrice = finalPrice,
+                        Stock = v.Stock
+                    });
+                }
+                Variants = new ObservableCollection<ProductVariantDisplay>(list);
+
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("Failed to load Product Details", ex);
+                Console.WriteLine($"Exception caught: {ex.Message}");
+            }
+        }
+    }
+}
