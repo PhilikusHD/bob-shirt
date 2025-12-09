e713bb88da292c1a776ea3dc7f3862d2660058b0
Roedba <niklas.dominico2@gmail.com>
2025-12-09 13:43:09 +0100

    Product Detail view in new Window #21

--- Bob.Core/ViewModels/ProductDetailWindowViewModel.cs
+++ Bob.Core/ViewModels/ProductDetailWindowViewModel.cs
@@ -2,27 +2,25 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Bob.Core.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Bob.Core.Logging;
 using Bob.Core.Domain;
 using Bob.Core.Models;
+
 namespace Bob.Core.ViewModels
 {
     public partial class ProductDetailWindowViewModel : ViewModelBase
     {
-
         [ObservableProperty]
         private Product m_CurrentProduct;
 
         [ObservableProperty]
-        private ObservableCollection<ProductVariantDisplay> m_Variants = [];
+        private ObservableCollection<ProductVariantDisplay> m_Variants = new();
+
         public ProductDetailWindowViewModel(Product product)
         {
-
-            // Load Caps asynchronously
             CurrentProduct = product;
             _ = LoadProductDetailAsync();
         }
@@ -33,31 +31,59 @@ namespace Bob.Core.ViewModels
             {
                 var variantEntities = await ProductService.GetVariantsForProductAsync(CurrentProduct.ProductId);
 
+                if (!variantEntities.Any())
+                {
+                    Variants.Clear();
+                    return;
+                }
+
+                var sizeIds = variantEntities.Select(v => v.SizeId).Distinct().ToList();
+                var colorIds = variantEntities.Select(v => v.ColorId).Distinct().ToList();
+
+                var sizeTasks = sizeIds.Select(id => ProductService.GetSizeAsync(id));
+                var colorTasks = colorIds.Select(id => ProductService.GetColorAsync(id));
+
+                await Task.WhenAll(
+                    Task.WhenAll(sizeTasks),
+                    Task.WhenAll(colorTasks)
+                );
+
+                var sizeDictionary = (await Task.WhenAll(sizeTasks))
+                    .Where(s => s != null)
+                    .ToDictionary(s => s.SizeId, s => s.SizeName);
+
+                var colorDictionary = (await Task.WhenAll(colorTasks))
+                    .Where(c => c != null)
+                    .ToDictionary(c => c.Id, c => c.ColorName);
+
+                var priceTasks = sizeIds.Select(async sizeId =>
+                {
+                    var price = await ProductService.GetPriceAdjustedForSize(sizeId, CurrentProduct.Price);
+                    return (SizeId: sizeId, Price: price);
+                }).ToList();
+
+                var priceResults = await Task.WhenAll(priceTasks);
+                var priceDictionary = priceResults.ToDictionary(p => p.SizeId, p => p.Price);
+
                 var list = new List<ProductVariantDisplay>();
 
                 foreach (var v in variantEntities)
                 {
-                    // Please do not make database calls inside a loop. 
-                    // Prefetch all sizes and colors, and the prices outside the loop and simply perform a lookup.
-                    // Check CalculateTotalPrice in System/CartSytem.cs for an example of this pattern.
-                    // k thx byeeeeeeeeeeeeeeeeeeeeeee.
-                    // Also... how the heck do you intend to wire this up to Caps, Hoodies and Tshirts? I'm very confused.
-                    // But I trust you have a plan here :D
-                    var size = await ProductService.GetSizeAsync(v.SizeId);
-                    var color = await ProductService.GetColorAsync(v.ColorId);
-                    var finalPrice = await ProductService.GetPriceAdjustedForSize(v.SizeId, CurrentProduct.Price);
+                    var sizeName = sizeDictionary.TryGetValue(v.SizeId, out var s) ? s : "Unknown";
+                    var colorName = colorDictionary.TryGetValue(v.ColorId, out var c) ? c : "Unknown";
+                    var finalPrice = priceDictionary.TryGetValue(v.SizeId, out var p) ? p : CurrentProduct.Price;
 
                     list.Add(new ProductVariantDisplay
                     {
                         VariantId = v.VariantId,
-                        Color = color.ColorName,
-                        Size = size.SizeName,
+                        Color = colorName,
+                        Size = sizeName,
                         FinalPrice = finalPrice,
                         Stock = v.Stock
                     });
                 }
-                Variants = new ObservableCollection<ProductVariantDisplay>(list);
 
+                Variants = new ObservableCollection<ProductVariantDisplay>(list);
             }
             catch (Exception ex)
             {
